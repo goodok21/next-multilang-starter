34b15e1df64321db781f805e4a67c81384f55b73
Alexander Erik <emmx@mail.ru>
2020-07-31 03:43:10 +0300

    initial

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@ const { nextI18NextRewrites } = require('next-i18next/rewrites');
 
 const localeSubpaths = { en: 'en', de: 'de' };
 
-module.exports = {
-  trailingSlash: false,
-  rewrites: async () => [...nextI18NextRewrites(localeSubpaths)],
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+
+    trailingSlash: false,
+    rewrites: async () => [...nextI18NextRewrites(localeSubpaths)],
+    publicRuntimeConfig: {
+      localeSubpaths,
+    },
+  };
 };
